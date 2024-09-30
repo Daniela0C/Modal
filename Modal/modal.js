f530f259dccb4e3c95ba780cdd99fe7708e0e2fc
Daniela0C <162722367+Daniela0C@users.noreply.github.com>
2024-09-29 21:54:53 -0300

    Add files via upload

---
+++ Modal/modal.js
@@ -0,0 +1,11 @@
+function abrirModal(){
+    var janelaModal = document.getElementById("janelaModal");
+
+    janelaModal.style.display = "block";
+}
+
+function fecharModal(){
+    var janelaModal = document.getElementById("janelaModal");
+
+    janelaModal.style.display = "none";
+}
